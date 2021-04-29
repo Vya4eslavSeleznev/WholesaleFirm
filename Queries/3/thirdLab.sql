2a80fc92845fa6171c9c51ed70ebbfeb057bb2a7
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2021-04-30 01:17:37 +0300

    cursor

--- Queries/3/thirdLab.sql
+++ Queries/3/thirdLab.sql
@@ -306,126 +306,99 @@ CLEAR SCREEN;
  --спроса между прогнозируемым на следующий день спросом и спросом на заданный товар последнего дня.
  --Таким образом, мы можем получить величину прогнозируемого спроса 
  
-    CREATE GLOBAL TEMPORARY TABLE DEMAND_TABLE(DAY NUMBER, GOOD_COUNT NUMBER)
-    ON COMMIT DELETE ROWS;
+   clear screen;
+--CREATE GLOBAL TEMPORARY TABLE DEMAND_TABLE(CREATE_DATE TIMESTAMP, GOOD_COUNT NUMBER)
+--ON COMMIT DELETE ROWS;
+--DROP TABLE DEMAND_TABLE
+
+CREATE OR REPLACE PROCEDURE DIAGRAM_FOR_GOOD1
+(DATE_FROM IN TIMESTAMP, DATE_TO IN TIMESTAMP, ID_GOOD IN NUMBER, RESULT OUT DOUBLE PRECISION)
+IS
+  TYPE DATA_CURSOR_TYPE IS REF CURSOR;
+  DATA_CURSOR DATA_CURSOR_TYPE;
+  
+  TYPE DEMAND_TABLE_TYPE IS TABLE OF DEMAND_TABLE%ROWTYPE; 
+  TMP_TABLE DEMAND_TABLE_TYPE;
+  
+  ROWS_COUNT NUMBER;
+  row_count NUMBER;
+BEGIN
+  DELETE FROM DEMAND_TABLE;
+
+  INSERT INTO DEMAND_TABLE (CREATE_DATE, GOOD_COUNT)
+	SELECT CREATE_DATE, SUM(GOOD_COUNT) AS SM
+    FROM SALES
+    WHERE CREATE_DATE >= DATE_FROM AND 
+    CREATE_DATE <= DATE_TO
+    AND GOOD_ID = ID_GOOD
+    GROUP BY CREATE_DATE
+    ORDER BY CREATE_DATE;
+  
+  SELECT COUNT(*)
+  INTO ROWS_COUNT
+  FROM DEMAND_TABLE;
     
-    CREATE OR REPLACE PROCEDURE DIAGRAM_FOR_GOOD
-    (DATE_FROM IN TIMESTAMP, DATE_TO IN TIMESTAMP, ID_GOOD IN NUMBER, RESULT OUT DOUBLE PRECISION, DATA_CURSOR OUT SYS_REFCURSOR)
-    IS
-        CURSOR GET_DATA IS
-            SELECT DAY, GOOD_COUNT
+  IF (ROWS_COUNT < 2) THEN 
+        RAISE_APPLICATION_ERROR(-20000, 'Not enough data');
+  END IF;
+  
+  LOOP 
+        --OPEN DATA_CURSOR FOR SELECT CREATE_DATE, SM FROM DEMAND_TABLE;
+        --FETCH DATA_CURSOR BULK COLLECT INTO TMP_TABLE;
+		
+        row_count := 0;
+        
+        SELECT COUNT(*)
+        INTO ROWS_COUNT
+        FROM DEMAND_TABLE;
+
+        EXIT WHEN (ROWS_COUNT = 2);
+        
+        OPEN DATA_CURSOR FOR
+        SELECT CREATE_DATE, GOOD_COUNT
+        FROM DEMAND_TABLE;
+        
+        DELETE FROM DEMAND_TABLE;
+            
+        LOOP
+            FETCH DATA_CURSOR BULK COLLECT INTO TMP_TABLE LIMIT 2;
+
+            SELECT COUNT(*)
+            INTO ROWS_COUNT
             FROM DEMAND_TABLE;
-    
-    ROWNUM_COUNT NUMBER;
-    CURRENT_ROWNUM NUMBER;
-    LAST_SUM NUMBER;
-    RES1 DOUBLE PRECISION;
-    RES2 DOUBLE PRECISION;
-    
-    MAX_GOOD_COUNT NUMBER;
-    
-    BEGIN
-        ROWNUM_COUNT := 0;
-        LAST_SUM := 0;
-        RES1 := 0;
-        RES2 := 0;
-        CURRENT_ROWNUM := 0;
-        
-        SELECT MAX(SUM(GOOD_COUNT))
-        INTO MAX_GOOD_COUNT
-        FROM SALES WHERE GOOD_ID = ID_GOOD
-        GROUP BY CREATE_DATE;
-        
-        INSERT INTO DEMAND_TABLE
-            SELECT CREATE_DATE, SUM(GOOD_COUNT) AS SM
-            FROM SALES
-            WHERE CREATE_DATE >= DATE_FROM AND 
-            CREATE_DATE <= DATE_TO
-            AND GOOD_ID = ID_GOOD
-            GROUP BY CREATE_DATE
-            ORDER BY CREATE_DATE;
-        
-        FOR CR IN GET_DATA
-        LOOP
-            
-            
-            
-        END LOOP;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        FOR CR IN GET_DATA
-        LOOP
-            ROWNUM_COUNT := ROWNUM_COUNT + 1;
-        END LOOP;
-        
-        FOR CR IN GET_DATA
-        LOOP
-            IF CURRENT_ROWNUM < ROWNUM_COUNT - 2 THEN
-                RES1 := RES1 + CR.SM;
-            ELSE
-                RES2 := RES2 + CR.SM;
+
+            IF (ROWS_COUNT = 1 AND DATA_CURSOR%ROWCOUNT - row_count = 1) THEN 
+                 INSERT INTO DEMAND_TABLE (CREATE_DATE, GOOD_COUNT)
+                 VALUES (TMP_TABLE(1).CREATE_DATE, TMP_TABLE(1).GOOD_COUNT);
             END IF;
             
-            CURRENT_ROWNUM := CURRENT_ROWNUM + 1;
+            row_count := DATA_CURSOR%ROWCOUNT;
+            EXIT WHEN DATA_CURSOR%NOTFOUND;
+			
+			INSERT INTO DEMAND_TABLE (CREATE_DATE, GOOD_COUNT)
+            VALUES (TMP_TABLE(1).CREATE_DATE, (TMP_TABLE(1).GOOD_COUNT + TMP_TABLE(2).GOOD_COUNT) / 2);
+			
+            SELECT COUNT(*)
+            INTO ROWS_COUNT
+            FROM DEMAND_TABLE;
+                   
         END LOOP;
+            
+        CLOSE DATA_CURSOR;   
+    END LOOP;
         
-        IF ROWNUM_COUNT > 2 THEN
-            RES1 := RES1 / (ROWNUM_COUNT - 2);
-            RES2 := RES2 / 2;
-        END IF;
-        
-        IF RES1 > RES2 THEN
-            RES1 := RES1 - RES2;
-        ELSE
-            RES1 := RES2 - RES1;
-        END IF;
-        
-        CURRENT_ROWNUM := 0;
-        
-        FOR CR IN GET_DATA
-        LOOP
-            IF CURRENT_ROWNUM = ROWNUM_COUNT - 1 THEN
-                LAST_SUM := CR.SM;
-            END IF;
-        END LOOP;
-        
-        RESULT := RES1 + LAST_SUM;
-    IF NOT DATA_CURSOR%ISOPEN THEN
+    OPEN DATA_CURSOR FOR SELECT CREATE_DATE, GOOD_COUNT FROM DEMAND_TABLE;
+    FETCH DATA_CURSOR BULK COLLECT INTO TMP_TABLE LIMIT 2;
+  
+    RESULT := ABS(TMP_TABLE(1).GOOD_COUNT - TMP_TABLE(2).GOOD_COUNT);
+END;
 
-    OPEN DATA_CURSOR FOR
-		SELECT CREATE_DATE, SUM(GOOD_COUNT) AS SM
-        FROM SALES
-        WHERE CREATE_DATE >= DATE_FROM AND 
-        CREATE_DATE <= DATE_TO
-        AND GOOD_ID = ID_GOOD
-        GROUP BY CREATE_DATE
-        ORDER BY CREATE_DATE;
-    END IF;
-    END;
-    
-    DROP PROCEDURE DIAGRAM_FOR_GOOD;
+DROP PROCEDURE DIAGRAM_FOR_GOOD1;
 
+SET SERVEROUTPUT ON;
+declare test double precision;
+begin
+    DIAGRAM_FOR_GOOD1('08.04.21', '15.04.21', 1, test);
+    DBMS_OUTPUT.PUT_LINE('AAAAA=' || test);
+end;
 
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    
